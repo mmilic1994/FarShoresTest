d947333445195f27739402bb6a4a3da5fdb70caa
mmilic1994 <mmilic1994@gmail.com>
2018-12-03 12:36:25 +0100

    2nd commit

---
+++ bootstrap/js/main.js
@@ -0,0 +1,12 @@
+const tire = 130;
+let tireOne = document.querySelector('#1_tire');
+let tireTwo = document.querySelector('#2_tire');
+let tireThree = document.querySelector('#3_tire');
+let tireFour = document.querySelector('#4_tire');
+
+document.addEventListener('DOMContentLoaded', ()=> {
+  
+
+   
+  
+})
